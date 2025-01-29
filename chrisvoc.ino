298fa51c1c3180eab8e4be8affcd8992b7250436
moeburn <moeburn@gmail.com>
2025-01-29 16:39:06 -0500

    add charts and buttons to show them

--- chrisvoc.ino
+++ chrisvoc.ino
@@ -2,7 +2,7 @@
 #include <SPI.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
-
+#include <Math.h>
 #include <Arduino.h>
 #include <Wire.h>
 
@@ -29,14 +29,28 @@ WebServer server(80);
 #define button3 20
 #define button4 7
 #define button5 5
+#define maxArray 501
+
+float tempOffset = -0.7;
+
+int sampleSecs = 30 * 1000;
+int brightness = 10;
+
+ float array1[maxArray];
+ float array2[maxArray];
+ float array3[maxArray];
+ float array4[maxArray];
 
 SensirionI2cSht4x sht4x;
 SensirionI2CSgp41 sgp41;
 Adafruit_MPU6050 mpu;
 VOCGasIndexAlgorithm voc_algorithm;
 NOxGasIndexAlgorithm nox_algorithm;
+ float minVal = 3.9;
+ float maxVal = 4.2;
+int readingCount = 0; // Counter for the number of readings
 int ldr_read;
-int button;
+int button = 5;
 // time in seconds needed for NOx conditioning
 uint16_t conditioning_s = 10;
 
@@ -53,7 +67,7 @@ uint16_t compensationT = 0;               // in ticks as defined by SGP41
 int32_t voc_index = voc_algorithm.process(srawVoc);
 int32_t nox_index = nox_algorithm.process(srawNox);
 float tempSHT, humSHT;
-
+unsigned long buttonTimer;
 TFT_eSPI tft = TFT_eSPI();       //for TFT display
 TFT_eSprite img = TFT_eSprite(&tft);
 
@@ -61,9 +75,11 @@ TFT_eSprite img = TFT_eSprite(&tft);
     static uint32_t __every__##interval = millis(); \
     if (millis() - __every__##interval >= interval && (__every__##interval = millis()))
 
-
-void doDisplay(){
   float scalingFactor = 1.2;
+
+
+void doMainDisplay(){
+
   img.fillSprite(TFT_BLACK);
   img.drawLine(119, 0, 119, 240, TFT_BLACK);  // Vertical center line
   img.drawLine(120, 0, 120, 240, TFT_BLACK); // Vertical center line (thicker)
@@ -78,7 +94,7 @@ void doDisplay(){
   img.print(button);
   img.setCursor(6 * scalingFactor, 40 * scalingFactor); // Centered vertically in quadrant
   img.setTextSize(3); // Font size 3 (24px)
-  img.print(temperature, 1);
+  img.print(tempSHT, 1);
   img.print("c");
 
   // Quadrant 2: Top-right
@@ -117,6 +133,202 @@ void doDisplay(){
 
 }
 
+void setupChart() {
+    img.setCursor(0, 0);
+    img.print("<");
+    img.print(maxVal, 3);
+    
+    img.setCursor(0, 229); // Adjusted for 240x240 display
+    img.print("<");
+    img.print(minVal, 3);
+    
+    img.setCursor(120, 229); // Adjusted for new width
+    img.print("<--");
+    img.print(readingCount - 1, 0);
+    img.print("*");
+    img.print(sampleSecs * 60, 0);
+    img.print("s-->");
+
+    
+
+    
+    img.setCursor(145, 0);
+}
+
+void doTempChart() {
+    setupChart();
+    minVal = array1[maxArray - readingCount];
+    maxVal = array1[maxArray - readingCount];
+
+    for (int i = maxArray - readingCount + 1; i < maxArray; i++) {
+        if (array1[i] != 0) {
+            if (array1[i] < minVal) minVal = array1[i];
+            if (array1[i] > maxVal) maxVal = array1[i];
+        }
+    }
+
+    // Adjust scaling factors
+    float yScale = 239.0 / (maxVal - minVal);
+    float xStep = 240.0 / (readingCount - 1);
+
+
+
+
+        img.fillRect(0, 0, img.width(), img.height(), TFT_WHITE);
+        
+        for (int i = maxArray - readingCount; i < (maxArray - 1); i++) {
+            int x0 = (i - (maxArray - readingCount)) * xStep;
+            int y0 = 239 - ((array1[i] - minVal) * yScale);
+            int x1 = (i + 1 - (maxArray - readingCount)) * xStep;
+            int y1 = 239 - ((array1[i + 1] - minVal) * yScale);
+
+            if (array1[i] != 0) {
+                img.drawLine(x0, y0, x1, y1, TFT_BLACK);
+            }
+        }
+        setupChart();
+        img.print("[Temp: ");
+        img.print(array1[(maxArray - 1)], 3);
+        img.print("c]");
+        img.pushSprite(0, 0);
+}
+
+void doHumChart() {
+    setupChart();
+    minVal = array2[maxArray - readingCount];
+    maxVal = array2[maxArray - readingCount];
+
+    for (int i = maxArray - readingCount + 1; i < maxArray; i++) {
+        if ((array2[i] < minVal) && (array2[i] > 0)) {
+            minVal = array2[i];
+        }
+        if (array2[i] > maxVal) {
+            maxVal = array2[i];
+        }
+    }
+
+    float yScale = 239.0 / (maxVal - minVal);
+    float xStep = 240.0 / (readingCount - 1);
+
+
+        img.fillRect(0, 0, img.width(), img.height(), TFT_WHITE);
+        
+        for (int i = maxArray - readingCount; i < (maxArray - 1); i++) {
+            int x0 = (i - (maxArray - readingCount)) * xStep;
+            int y0 = 239 - ((array2[i] - minVal) * yScale);
+            int x1 = (i + 1 - (maxArray - readingCount)) * xStep;
+            int y1 = 239 - ((array2[i + 1] - minVal) * yScale);
+            
+            if (array2[i] > 0) {
+                img.drawLine(x0, y0, x1, y1, TFT_BLACK);
+            }
+        }
+        setupChart();
+        img.print("[Hum: ");
+        img.print(array2[(maxArray - 1)], 0);
+        img.print("%]");
+        img.pushSprite(0, 0);
+}
+
+void doVocChart() {
+    setupChart();
+    minVal = array3[maxArray - readingCount];
+    maxVal = array3[maxArray - readingCount];
+
+    for (int i = maxArray - readingCount + 1; i < maxArray; i++) {
+        if ((array3[i] < minVal) && (array3[i] > 0)) {
+            minVal = array3[i];
+        }
+        if (array3[i] > maxVal) {
+            maxVal = array3[i];
+        }
+    }
+
+    float yScale = 239.0 / (maxVal - minVal);
+    float xStep = 240.0 / (readingCount - 1);
+
+
+        img.fillRect(0, 0, img.width(), img.height(), TFT_WHITE);
+        
+        for (int i = maxArray - readingCount; i < (maxArray - 1); i++) {
+            int x0 = (i - (maxArray - readingCount)) * xStep;
+            int y0 = 239 - ((array3[i] - minVal) * yScale);
+            int x1 = (i + 1 - (maxArray - readingCount)) * xStep;
+            int y1 = 239 - ((array3[i + 1] - minVal) * yScale);
+
+            if (array3[i] > 0) {
+                img.drawLine(x0, y0, x1, y1, TFT_BLACK);
+            }
+        }
+        setupChart();
+        img.print("[VOC Index: ");
+        img.print(array3[(maxArray - 1)], 2);
+        img.print("]");
+        img.pushSprite(0, 0);
+}
+
+void doNoxChart() {
+    setupChart();
+    minVal = array4[maxArray - readingCount];
+    maxVal = array4[maxArray - readingCount];
+
+    for (int i = maxArray - readingCount + 1; i < maxArray; i++) {
+        if ((array4[i] < minVal) && (array4[i] > 0)) {
+            minVal = array4[i];
+        }
+        if (array4[i] > maxVal) {
+            maxVal = array4[i];
+        }
+    }
+
+    float yScale = 239.0 / (maxVal - minVal);
+    float xStep = 240.0 / (readingCount - 1);
+
+
+        img.fillRect(0, 0, img.width(), img.height(), TFT_WHITE);
+        
+        for (int i = maxArray - readingCount; i < (maxArray - 1); i++) {
+            int x0 = (i - (maxArray - readingCount)) * xStep;
+            int y0 = 239 - ((array4[i] - minVal) * yScale);
+            int x1 = (i + 1 - (maxArray - readingCount)) * xStep;
+            int y1 = 239 - ((array4[i + 1] - minVal) * yScale);
+
+            if (array4[i] > 0) {
+                img.drawLine(x0, y0, x1, y1, TFT_BLACK);
+            }
+        }
+        setupChart();
+        img.print("[Raw NOX: ");
+        img.print(array4[(maxArray - 1)], 2);
+        img.print("]");
+        img.pushSprite(0, 0);
+}
+
+void takeSamples(){
+        if (readingCount < maxArray) {
+            readingCount++;
+        }
+
+        for (int i = 0; i < (maxArray - 1); i++) {
+            array1[i] = array1[i + 1];
+        }
+        array1[(maxArray - 1)] = tempSHT;
+
+        for (int i = 0; i < (maxArray - 1); i++) {
+            array2[i] = array2[i + 1];
+        }
+        array2[(maxArray - 1)] = humidity;
+        
+        for (int i = 0; i < (maxArray - 1); i++) {
+            array3[i] = array3[i + 1];
+        }
+        array3[(maxArray - 1)] = voc_index;
+
+        for (int i = 0; i < (maxArray - 1); i++) {
+            array4[i] = array4[i + 1];
+        }
+        array4[(maxArray - 1)] = srawNox;
+}
 
 void setup() {
 
@@ -133,7 +345,7 @@ void setup() {
   tft.init();
   tft.setRotation(0);
   tft.fillScreen(TFT_BLACK);
-tft.drawRect(0,0, tft.width(), tft.height(), TFT_WHITE);
+  tft.drawRect(0,0, tft.width(), tft.height(), TFT_WHITE);
 
   tft.setCursor(5,5);
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
@@ -218,7 +430,7 @@ tft.drawRect(0,0, tft.width(), tft.height(), TFT_WHITE);
         compensationT = defaultCompenstaionT;
         Serial.flush();
     } else {
-        tempSHT = temperature;
+        tempSHT = temperature + tempOffset;
         humSHT = humidity;
         // convert temperature and humidity to ticks as defined by SGP41
         // interface
@@ -259,33 +471,63 @@ void loop() {
     int newldr;
     ldr_read = analogRead(ldr_pin);
     newldr = map(ldr_read, 0, 4096, 0, 255);
+    newldr = newldr + brightness;
     if (newldr < 1) {newldr=1;}
+    if (newldr > 255) {newldr=255;}
     analogWrite(bkl_pin, newldr);
-    if (!digitalRead(button1)) {button = 1;}
-    if (!digitalRead(button2)) {button = 2;}
-    if (!digitalRead(button3)) {button = 3;}
-    if (!digitalRead(button4)) {button = 4;}
-    if (!digitalRead(button5)) {button = 5;}
+    if (!digitalRead(button1)) {button = 1;} //towards
+    if (!digitalRead(button2)) {button = 2;} //down
+    if (!digitalRead(button3)) {button = 3;} //away
+    if (!digitalRead(button4)) {button = 4;} //up
+    if (!digitalRead(button5)) {                //pressed
+        buttonTimer = millis();
+        button = 5;
+        while (!digitalRead(5))
+        {
+          delay(10);
+          if ((millis() - buttonTimer) > 2000) {
+            button = 6;
+            return;
+          }
+        }
+        
+      } 
     if (abs(y) > abs(x)) {
       if (y > 0) {
         tft.setRotation(3); // Up
-        Serial.println("Orientation: UP");
+        //Serial.println("Orientation: UP");
       } else {
         tft.setRotation(1); // Down
-        Serial.println("Orientation: DOWN");
+        //Serial.println("Orientation: DOWN");
       }
     } else {
       if (x > 0) {
         tft.setRotation(0); // Right
-        Serial.println("Orientation: RIGHT");
+        //Serial.println("Orientation: RIGHT");
       } else {
         tft.setRotation(2); // Left
-        Serial.println("Orientation: LEFT");
+        //Serial.println("Orientation: LEFT");
       }
     }
   }
 
   every(1000){
+    switch (button) {
+      case 1: 
+        doTempChart();
+        break;
+      case 2: 
+        doHumChart(); 
+        break;
+      case 3: 
+        doVocChart();
+        break;
+      case 4: 
+        doNoxChart();
+        break;
+      case 5: 
+        doMainDisplay();
+    }
 
     // 3. Measure SGP4x signals
     if (conditioning_s > 0) {
@@ -310,7 +552,6 @@ void loop() {
          voc_index = voc_algorithm.process(srawVoc);
          nox_index = nox_algorithm.process(srawNox);
     }
-    doDisplay();
   }
 
   every (15000){
@@ -326,17 +567,16 @@ void loop() {
         compensationT = defaultCompenstaionT;
         Serial.flush();
     } else {
-        tempSHT = temperature;
+        tempSHT = temperature + tempOffset;
         humSHT = humidity;
-        // convert temperature and humidity to ticks as defined by SGP41
-        // interface
-        // NOTE: in case you read RH and T raw signals check out the
-        // ticks specification in the datasheet, as they can be different for
-        // different sensors
         compensationT = static_cast<uint16_t>((tempSHT + 45) * 65535 / 175);
         compensationRh = static_cast<uint16_t>(humSHT * 65535 / 100);
     }
 
+  }
+
+  every (sampleSecs) {
+    takeSamples();
   }
 
 }
